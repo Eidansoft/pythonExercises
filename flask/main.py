3fe5d72d44d3fcdbcb61076921d7faef98f3ce64
Alejandro Lorente <alejandro.lorente@qvantel.com>
2017-07-27 12:46:59 +0200

    add log method

--- flask/main.py
+++ flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, escape
 from searchLetters import search4letters, search4vowels
 
 app = Flask(__name__)
@@ -10,12 +10,29 @@ def mainPage():
     return render_template('entry.html', title='Welcome!!!')
 
 @app.route('/search', methods=['POST'])
-def searchLettersIntoPhrase():
-    phrase='prueba de concepto'
-    letters='aeiou'
-#    res=search4letters(phrase, letters)
+def search_letters_into_phrase():
+    phrase=request.form['phrase']
+    letters=request.form['letters']
     res=search4letters(phrase, letters)
-#    return render_template('result.html', title='Results!!!', phrase=phrase, letters=letters, result=res)
-    return res
+    log_request(request, res)        
+    return render_template('result.html', title='Results!!!', phrase=phrase, letters=letters, result=res)
 
-app.run()
+@app.route('/log')
+def get_log():
+    with open('log.txt') as log:
+        res=log.read()
+
+    return escape(res)
+    
+def log_request(req, res):
+    with open('log.txt', 'a') as log:
+        log.write(str(req.form))
+        log.write(str(req.remote_addr))
+        log.write(str(req.user_agent))
+        log.write(str(res))
+        log.write('\n')
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
