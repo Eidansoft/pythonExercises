4c4c8a8f05396b2fc5430032c03f2744cd69927f
Eidansoft <eidansoft@gmail.com>
2017-07-31 09:47:54 +0200

    Add new views to poll app

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -8,3 +8,13 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('Hola mundo a Django')
 
+def detail(request, question_id):
+    return HttpResponse('Esta es la pregunta %s.' % question_id)
+
+def results(request, question_id):
+    response = 'Esto son los resultados de la encuesta %s.'
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('Estas votando la pregunta %s.' % question_id)
+
